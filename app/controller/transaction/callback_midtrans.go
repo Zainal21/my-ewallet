18a7d765b529288da4edaedf5bf97e978617d835
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2024-01-30 11:04:06 +0700

    pref: optimize callback process

--- app/controller/transaction/callback_midtrans.go
+++ app/controller/transaction/callback_midtrans.go
@@ -41,6 +41,7 @@ type PaymentNotificationCallback struct {
 }
 
 // Serve implements contract.Controller.
+// Serve implements contract.Controller.
 func (g *callbackMidtransImpl) Serve(xCtx appctx.Data) appctx.Response {
 	ctx := xCtx.FiberCtx
 	body := new(PaymentNotificationCallback)
@@ -50,17 +51,23 @@ func (g *callbackMidtransImpl) Serve(xCtx appctx.Data) appctx.Response {
 		return helpers.CreateErrorResponse(fiber.StatusBadRequest, "INVALID PAYLOAD", nil)
 	}
 	logger.Info(fmt.Sprintf("body callback request %v", body))
-	transactionStatus := body.TransactionStatus
-	status := "process"
 
-	if transactionStatus == "pending" {
+	var (
+		status         string
+		transactionLog bool
+	)
+
+	switch body.TransactionStatus {
+	case "pending":
 		status = "process"
-		logger.Info(fmt.Sprintf("status transaction pending %v", transactionStatus))
-	}
-
-	if transactionStatus == "deny" || transactionStatus == "cancel" || transactionStatus == "expire" || transactionStatus == "failure" {
+		logger.Info(fmt.Sprintf("status transaction pending %v", body.TransactionStatus))
+	case "deny", "cancel", "expire", "failure":
 		status = "failed"
-		logger.Info(fmt.Sprintf("status transaction cancel/expire/deny/failure %v", transactionStatus))
+		logger.Info(fmt.Sprintf("status transaction cancel/expire/deny/failure %v", body.TransactionStatus))
+	case "settlement", "capture":
+		status = "success"
+		transactionLog = true
+		logger.Info(fmt.Sprintf("status transaction success %v", body.TransactionStatus))
 	}
 
 	trns, err := g.transSrv.GetTransactionByFieldName(ctx.Context(), "order_id", body.OrderID)
@@ -75,13 +82,10 @@ func (g *callbackMidtransImpl) Serve(xCtx appctx.Data) appctx.Response {
 		return helpers.CreateErrorResponse(fiber.StatusInternalServerError, consts.ServerErrorMessage, nil)
 	}
 
-	if transactionStatus == "settlement" || transactionStatus == "capture" {
-		status = "success"
-		logger.Info(fmt.Sprintf("status transaction success %v", transactionStatus))
-		var currentBalance int = 0
+	if transactionLog {
+		var currentBalance int
 
 		balance, err := g.transSrv.GetBalance(ctx.Context(), "user_id", trns.UserID)
-
 		err = helpers.HandleError(err)
 		if err != nil {
 			return helpers.CreateErrorResponse(fiber.StatusInternalServerError, consts.ServerErrorMessage, nil)
@@ -91,15 +95,20 @@ func (g *callbackMidtransImpl) Serve(xCtx appctx.Data) appctx.Response {
 			currentBalance, _ = strconv.Atoi(balance.FinalDeposit)
 		}
 
+		grossAmount, err := strconv.ParseFloat(body.GrossAmount, 64)
+		err = helpers.HandleError(err)
+		if err != nil {
+			return helpers.CreateErrorResponse(fiber.StatusInternalServerError, consts.ServerErrorMessage, nil)
+		}
+
 		// create deposit log
-		grossAmount, _ := strconv.Atoi(body.GrossAmount)
 		err = g.transSrv.CreateDepositLog(ctx.Context(), dtos.LedgerDto{
 			UserID:         trns.UserID,
 			RefID:          body.ReferenceID,
 			Type:           "TOP UP",
 			CurrentDeposit: currentBalance,
-			ChangeDeposit:  "+ " + body.GrossAmount,
-			FinalDeposit:   currentBalance + grossAmount,
+			ChangeDeposit:  fmt.Sprintf("+ %.2f", grossAmount),
+			FinalDeposit:   currentBalance + int(grossAmount),
 			Note:           "TOP UP with REF ID : " + body.ReferenceID,
 		})
 		err = helpers.HandleError(err)
